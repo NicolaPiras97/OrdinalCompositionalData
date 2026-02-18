fea33a2244d7efbc17eb32cec7294f3fbeb71c78
NicolaPiras97 <npiras1997@gmail.com>
2026-02-18 12:26:42 +0100

    Update Educ.R

--- Educ.R
+++ Educ.R
@@ -218,3 +218,143 @@ for(i in 1:N){
 R2C<-SSRc/denC
 cat("\n--- KLD R-SQUARED ---\n")
 print(R2C*((N-(Cx*(Cy-1)))/N))
+
+##############BOOTSTRAP USING WASSERSTEIN ORDER#################
+weightsB<-list()
+Btot<-list()
+vartotc<-rep(0,Cy)
+for(b in 1:B){
+xB<-matrix(0,nrow=N,ncol=Cx)
+yB<-matrix(0,nrow=N,ncol=Cy)
+rid<-matrix(0,nrow=N,ncol=Cy)
+indexB<-sample(1:N,N,replace=T)
+for(i in 1:N){
+xB[i,]<-x[indexB[i],]
+}
+for(i in 1:N){
+  yB[i,]<-y[indexB[i],]
+}
+
+
+ycdf<-matrix(0,nrow=N,ncol=Cy)
+for(i in 1:N){
+  ycdf[i,]<-cumsum(yB[i,])
+}
+weightsB[[b]]<-c(median(ycdf[,2])-median(ycdf[,1]),median(ycdf[,3])-median(ycdf[,2]))
+
+ydataB<-list()
+xdataB<-list()
+for(i in 1:N){
+  ydataB[[i]]<-yB[i,]
+}
+for(i in 1:N){
+  xdataB[[i]]<-xB[i,]
+}
+solB<-solve_simplex_lp( xdataB , ydataB , weightsB[[b]] )
+Btot[[b]]<-solB$A
+
+for(j in 1:Cy){
+  vartotc[j]=vartotc[j]+wd(weightsB[[b]],A_hat[,j],Btot[[b]][,j])
+}
+
+}
+
+#95% confidence region
+distotB<-matrix(0,nrow=B,ncol=Cy)
+for(i in 1:B){
+  for(j in 1:B){
+    if(i!=j){
+    for(k in 1:Cy){
+    if(opiwd(weightsB[[b]],Btot[[i]][,k],Btot[[j]][,k])==2){
+      distotB[i,k]=distotB[i,k]+1
+    }
+    }
+    }
+  }
+}
+ordtotB<-matrix(0,nrow=B,ncol=Cy)
+for(k in 1:Cy){
+  i=1
+for(j in 1:B){
+  if(length(which(distotB[,k]==sort(distotB[,k],decreasing = F)[i]))==1){
+    ordtotB[i,k]<-which(distotB[,k]==sort(distotB[,k],decreasing = F)[i])
+  }
+  if(length(which(distotB[,k]==sort(distotB[,k],decreasing = F)[i]))>1){
+    ordtotB[i:(i+length(which(distotB[,k]==sort(distotB[,k],decreasing = F)[i]))-1),k]<-which(distotB[,k]==sort(distotB[,k],decreasing = F)[i])
+  }
+  i=i+length(which(distotB[,k]==sort(distotB[,k],decreasing = F)[i]))
+  if(length(ordtotB[,k]==B)){
+    stop
+  }
+}
+}
+bmedianw<-matrix(0,nrow=Cx,ncol=Cy)
+if(B%%2==1){
+  for(k in 1:Cy){
+     bmedianw[,k]<-Btot[[ordtotB[round(B/2),k]]][,k]
+  }
+}
+if(B%%2==0){
+  for(k in 1:Cy){
+  bmedianw[,k]<-apply(cbind(Btot[[ordtotB[B/2,k]]][,k],Btot[[ordtotB[B/2+1,k]]][,k]),1,mean)
+  }
+}
+
+binfw<-matrix(0,nrow=Cx,ncol=Cy)
+if(B%%2==1){
+  for(k in 1:Cy){
+    binfw[,k]<-Btot[[ordtotB[round(B/40),k]]][,k]
+  }
+}
+if(B%%2==0){
+  for(k in 1:Cy){
+    binfw[,k]<-apply(cbind(Btot[[ordtotB[B/40,k]]][,k],Btot[[ordtotB[B/40+1,k]]][,k]),1,mean)
+  }
+}
+bsupw<-matrix(0,nrow=Cx,ncol=Cy)
+if(B%%2==1){
+  for(k in 1:Cy){
+    bsupw[,k]<-Btot[[ordtotB[round(B/1.02555),k]]][,k]
+  }
+}
+if(B%%2==0){
+  for(k in 1:Cy){
+    bsupw[,k]<-apply(cbind(Btot[[ordtotB[B/1.02555,k]]][,k],Btot[[ordtotB[B/1.02555+1,k]]][,k]),1,mean)
+  }
+}
+
+bq1w<-matrix(0,nrow=Cx,ncol=Cy)
+if(B%%2==1){
+  for(k in 1:Cy){
+    bq1w[,k]<-Btot[[ordtotB[round(B/4),k]]][,k]
+  }
+}
+if(B%%2==0){
+  for(k in 1:Cy){
+    bq1w[,k]<-apply(cbind(Btot[[ordtotB[B/4,k]]][,k],Btot[[ordtotB[B/4+1,k]]][,k]),1,mean)
+  }
+}
+
+bq3w<-matrix(0,nrow=Cx,ncol=Cy)
+if(B%%2==1){
+  for(k in 1:Cy){
+    bq3w[,k]<-Btot[[ordtotB[round(B/1.33333),k]]][,k]
+  }
+}
+if(B%%2==0){
+  for(k in 1:Cy){
+    bq3w[,k]<-apply(cbind(Btot[[ordtotB[B/1.33333,k]]][,k],Btot[[ordtotB[B/1.33333+1,k]]][,k]),1,mean)
+  }
+}
+
+cat("\n--- Bootstrap median ---\n")
+print(round(bmedianw, 4))
+cat("\n--- Bootstrap inf ---\n")
+print(round(binfw, 4))
+cat("\n--- Bootstrap sup ---\n")
+print(round(bsupw, 4))
+cat("\n--- Bootstrap q1 ---\n")
+print(round(bq1w, 4))
+cat("\n--- Bootstrap q3 ---\n")
+print(round(bq3w, 4))
+                        
